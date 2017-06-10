c2aa73c729d937c82431eb50abf9e8fb476bd118
Keryann Malacchina <keryannm@gmail.com>
2017-06-10 20:45:57 +0200

    Stun, Stuck, Push, Hit, Death ...

--- Assets/SCRIPTS/Enemies/Enemy.cs
+++ Assets/SCRIPTS/Enemies/Enemy.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using DG.Tweening;
+using Sirenix.OdinInspector;
+
+public enum EnemyState { Normal, Stunned, Stuck, Pushed, Dead };
 
 public class Enemy : MonoBehaviour 
 {
+	public EnemyState enemyState = EnemyState.Normal;
+	public LayerMask wallLayer;
+
 	[Header ("Health")]
 	[Range (0, 100)]
 	public float health = 5;
 
 	[Header ("Speed")]
 	public float speed = 10;
+
+	[Header ("LookAt")]
 	public float lookAtLerp = 0.5f;
 
-	[Header ("Update")]
-	public float updateDelay = 0.1f;
+	[Header ("Follow")]
+	public float followUpdateDelay = 0.1f;
 
 	[Header ("Debug")]
 	public Transform target;
+	public float distanceFromTarget;
 	public bool debugLog = true;
 
 	private float _initialSpeed;
 	private float _initialLookAtLerp;
 	private NavMeshAgent _navMeshAgent;
 	private Transform _player;
+	private Rigidbody _rigidbody;
 
 	// Use this for initialization
 	void Start () 
@@ -34,26 +44,55 @@ public class Enemy : MonoBehaviour
 
 	public void Setup ()
 	{
+		enemyState = EnemyState.Normal;
+
 		_player = GameObject.FindGameObjectWithTag ("Player").transform;
 
+		_rigidbody = GetComponent<Rigidbody> ();
 		_navMeshAgent = GetComponent<NavMeshAgent> ();
 		_initialSpeed = speed;
 		_initialLookAtLerp = lookAtLerp;
 
 		_navMeshAgent.speed = speed;
 
-		StartCoroutine (SetDestinationUpdate ());
+		StartCoroutine (SetFollowUpdate ());
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-		
+		LookAt ();
+
+		DistanceFromTarget ();
+	}
+
+	void FixedUpdate () 
+	{
+
 	}
 
 	void LookAt ()
 	{
-		
+		if (target == null)
+			return;
+
+		if (enemyState == EnemyState.Stunned)
+			return;
+
+
+		Vector3 targetPosition = target.position;
+		targetPosition.y = transform.position.y;
+
+		Quaternion rotation = Quaternion.LookRotation (targetPosition - transform.position);
+
+		transform.rotation = Quaternion.Lerp (transform.rotation, rotation, lookAtLerp);
+	}
+
+	void DistanceFromTarget ()
+	{
+		if (target == null)
+			distanceFromTarget = -1;
+		else
+			distanceFromTarget = Vector3.Distance (transform.position, target.position);
 	}
 
 	void OnCollisionEnter (Collision collision)
@@ -63,6 +102,69 @@ public class Enemy : MonoBehaviour
 			if(debugLog)
 				Debug.Log (name + " : player hit!");
 		}
+
+		if(collision.gameObject.tag == "Wall" || (wallLayer.value & 1<<collision.gameObject.layer) == 1 <<collision.gameObject.layer)
+		{
+			if(debugLog)
+				Debug.Log (name + " : wall hit!");
+		}
+	}
+
+	public void Push (Vector3 direction, float force, ForceMode forcemode)
+	{
+		enemyState = EnemyState.Pushed;
+
+		Stuck (false);
+
+		_rigidbody.AddForce (direction* force, forcemode);
+
+		StartCoroutine (WaitZeroVelocity (()=> Unstuck ()));
+	}
+
+	public void Push (Vector3 direction, float force, ForceMode forcemode, float duration)
+	{
+		enemyState = EnemyState.Pushed;
+
+		_navMeshAgent.enabled = false;
+
+		_rigidbody.isKinematic = false;
+
+		_rigidbody.AddForce (direction* force, forcemode);
+
+		DOVirtual.DelayedCall (duration, Unstuck);
+	}
+
+	public void Stun (float duration)
+	{
+		enemyState = EnemyState.Stunned;
+
+		Stuck (false);
+
+		DOVirtual.DelayedCall (duration, Unstuck);
+	}
+
+	public void Stuck (bool changeState = true)
+	{
+		if(changeState)
+			enemyState = EnemyState.Stuck;
+
+		_navMeshAgent.enabled = false;
+		_rigidbody.isKinematic = false;
+	}
+
+	public void Unstuck ()
+	{
+		enemyState = EnemyState.Normal;
+
+		_rigidbody.isKinematic = true;
+		_navMeshAgent.enabled = true;
+	}
+
+	public IEnumerator WaitZeroVelocity (System.Action action)
+	{
+		yield return new WaitUntil (() => _rigidbody.velocity.magnitude < 1.5f);
+
+		action ();
 	}
 
 	public void Hit (int damage)
@@ -84,18 +186,19 @@ public class Enemy : MonoBehaviour
 		Destroy (gameObject);
 	}
 
-	public void SetDestination (Transform target, float duration)
+	public void SetDestinationTemporarily (Transform target, float duration)
 	{
 		_navMeshAgent.SetDestination (target.position);
 
 		this.target = target;
 
-		StopCoroutine (SetDestinationUpdate ());
-		StartCoroutine (SetDestinationUpdate ());
+		StopCoroutine (SetFollowUpdate ());
+		StartCoroutine (SetFollowUpdate ());
 
 		DOVirtual.DelayedCall (duration, ()=> SetPlayerDestination ());
 	}
 
+	[ButtonAttribute("Follow Player")]
 	public void SetPlayerDestination ()
 	{
 		if(_player == null)
@@ -103,8 +206,8 @@ public class Enemy : MonoBehaviour
 
 		target = _player;
 
-		StopCoroutine (SetDestinationUpdate ());
-		StartCoroutine (SetDestinationUpdate ());
+		StopCoroutine (SetFollowUpdate ());
+		StartCoroutine (SetFollowUpdate ());
 
 		_navMeshAgent.SetDestination (_player.position);
 	}
@@ -130,15 +233,19 @@ public class Enemy : MonoBehaviour
 		lookAtLerp = _initialLookAtLerp;
 	}
 
-	IEnumerator SetDestinationUpdate ()
+	IEnumerator SetFollowUpdate ()
 	{
 		if (target == null)
 			yield return new WaitUntil (()=> target != null);
 
-		_navMeshAgent.SetDestination (target.position);
+		if(_navMeshAgent.enabled)
+		{
+			if(distanceFromTarget > _navMeshAgent.stoppingDistance)
+				_navMeshAgent.SetDestination (target.position);
+		}
 
-		yield return new WaitForSeconds (updateDelay);
+		yield return new WaitForSeconds (followUpdateDelay);
 
-		StartCoroutine (SetDestinationUpdate ());
+		StartCoroutine (SetFollowUpdate ());
 	}
 }
