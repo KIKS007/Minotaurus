582715744dc1728c26b9e4823d0e792507b4906c
Unknown <t.monnet@rubika-edu.com>
2017-06-11 00:49:24 +0200

    Add ammo system

--- Assets/SCRIPTS/Weapon.cs
+++ Assets/SCRIPTS/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour {
     public Transform Canon;
     public Ammo Mun1;
     public Ammo Mun2;
+    public int ammo1;
+    public int ammo2;
     private float _cooldown1 = 0f;
     private float _cooldown2 = 0f;
 
@@ -27,10 +29,11 @@ public class Weapon : MonoBehaviour {
         {
             if (Input.GetButton("Fire1"))
             {
-                if (Mun1 != null)
+                if (Mun1 != null && ammo1 > 0)
                 {
                     Instantiate(Mun1, Canon.position, transform.rotation);
                     _cooldown1 = Mun1.cooldown;
+                    ammo1--;
                 }
             }
         }
@@ -43,10 +46,11 @@ public class Weapon : MonoBehaviour {
         {
             if (Input.GetButton("Fire2"))
             {
-                if (Mun2 != null)
+                if (Mun2 != null && ammo2 > 0)
                 {
                     Instantiate(Mun2, Canon.position, transform.rotation);
                     _cooldown2 = Mun2.cooldown;
+                    ammo2--;
                 }
             }
         }
