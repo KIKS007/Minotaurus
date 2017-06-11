d5bcf8ef923acac23fb3408ab68d594565280cad
Thibaut Monnet <ThibautM60@wanadoo.fr>
2017-06-11 18:10:06 +0200

    Update + Drink + Door

---
+++ Assets/SCRIPTS/DoorManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorManager : MonoBehaviour {
+
+    private float posYDrink;
+    public GameObject door;
+    public float timebeforedown = 2;
+
+	// Use this for initialization
+	void Start () {
+        posYDrink = transform.position.y;
+	}
+	
+	// Update is called once per frame  
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            door.transform.DOMoveY(posYDrink + transform.localScale.y + 0.5f, 1, false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+       if(other.tag == "Player")
+        {
+            DOVirtual.DelayedCall(timebeforedown, () => door.transform.DOMoveY(posYDrink, 1, false));
+        }
+    }
+}
---
+++ Assets/SCRIPTS/HotDrinkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotDrinkManager : MonoBehaviour {
+
+    public GameObject drink;
+    public Transform posDrink;
+
+    public int nbmin_drink = 2;
+    public int nbmax_drink = 5;
+
+    public void UseHotDrink()
+    {
+        int nb_drink = Random.Range(nbmin_drink, nbmax_drink + 1);
+        for (int i = 0; i < nb_drink; i++)
+        {
+            Instantiate(drink, posDrink.position, drink.transform.rotation);
+        }
+    }
+}
